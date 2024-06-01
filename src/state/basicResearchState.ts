bc0c26e854b402cc70dff32d709d78daf4c9e544
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2024-06-02 01:23:54 +0900

    stateの追加

--- src/state/basicResearchState.ts
+++ src/state/basicResearchState.ts
@@ -20,3 +20,53 @@ export const searchRangeState = atom<number>({
     key: 'searchRange',
     default: undefined
 });
+
+export const smallAreaState = atom<number>({
+    key: 'smallArea',
+    default: undefined
+});
+
+export const middleAreaState = atom<number>({
+    key: 'middleArea',
+    default: undefined
+});
+
+export const largeAreaState = atom<number>({
+    key: 'largeArea',
+    default: undefined
+});
+
+export const datumState = atom<"world"|"tokyo">({
+    key: 'datum',
+    default: "world"
+});
+
+export const freeDrinkState = atom<boolean>({
+    key: 'freeDrink',
+    default: undefined
+});
+
+export const freeFoodState = atom<boolean>({
+    key: 'freeFood',
+    default: undefined
+});
+
+export const privateRoomState = atom<boolean>({
+    key: 'privateRoom',
+    default: undefined
+});
+
+export const cardState = atom<boolean>({
+    key: 'card',
+    default: undefined
+});
+
+export const nonSmokingState = atom<boolean>({
+    key: 'nonSmoking',
+    default: undefined
+});
+
+export const parkingState = atom<boolean>({
+    key: 'parkingState',
+    default: undefined
+});
