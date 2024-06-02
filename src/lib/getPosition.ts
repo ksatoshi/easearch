d4fd09991dab7c74e001e41ee08e1bee2fa4a05e
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2024-06-02 14:53:42 +0900

    位置情報取得関数にlocalStorageに座標を保存する処理を追加

--- src/lib/getPosition.ts
+++ src/lib/getPosition.ts
@@ -1,12 +1,18 @@
+import { userLatState, userLonState } from "@/state/userState";
+import { useRecoilState } from "recoil";
+import { saveToLocalStorage } from "./localStoratgeControler";
+
 export function getPosition(){
     if(navigator === null) return null;
 
     navigator.geolocation.getCurrentPosition(
         (position) =>{
-           // setUserLat(position.coords.latitude);
-            console.log('user lat is:',position.coords.latitude);
-           // setUserLon(position.coords.longitude);
-            console.log('user lon:',position.coords.longitude);
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            // ここら辺よくわからないのでとりあえずいったんlocalstorageに保存する
+            saveToLocalStorage('lat',lat.toString());
+            saveToLocalStorage('lon',lon.toString());
         }
     );
 }
